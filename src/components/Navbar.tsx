b5b54efeafbec3f20d6aa100ec6d9101a688fde6
alexchvod <uceiac8@ucl.ac.uk>
2025-05-31 23:05:06 +0100

    add pets to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -103,6 +103,21 @@ export default function Navbar() {
                   Generate
                 </span>
               </Link>
+              <Link
+  href="/pets"
+  className={`mr-6 py-2 font-medium text-[15px] ${
+    pathname === '/pets'
+      ? 'text-[#8A4FFF] active'
+      : 'text-[#667085] hover:text-[#8A4FFF]'
+  }`}
+>
+  <span className="flex items-center">
+    <svg className="w-5 h-5 mr-1.5" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M6 10a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm6-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm6 3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 14c0 3.314 4 6 4 6s4-2.686 4-6a4 4 0 0 0-8 0z" />
+    </svg>
+    Pets
+  </span>
+</Link>
             </div>
           </div>
           <div className="flex items-center">
