e8d49f9544a76ade57c1194c75d0ae371cd18942
alexchvod <uceiac8@ucl.ac.uk>
2025-06-01 10:33:38 +0100

    button color

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -116,7 +116,7 @@ export default function SignUp() {
             <button
               type="submit"
               disabled={isLoading}
-              className="paw-button flex w-full justify-center rounded-full bg-pet-purple hover:bg-pet-purple-light px-4 py-3 text-sm font-medium text-white shadow-lg transition-all"
+              className="paw-button flex w-full justify-center rounded-full bg-black hover:bg-pet-purple-light px-4 py-3 text-sm font-medium text-white shadow-lg transition-all"
             >
               {isLoading ? 'Signing up...' : 'Sign Up'}
             </button>
